a93bdb3d3cadcdbfeb350cc3c0034e8a49b8fdd4
NuCash <39842364+nucash-mining@users.noreply.github.com>
2025-08-17 02:00:34 -0500

    Fix missing setShowMiningPoolModal state variable

--- src/components/MiningRigBuilder.tsx
+++ src/components/MiningRigBuilder.tsx
@@ -110,6 +110,7 @@ const MiningRigBuilder: React.FC = () => {
   const [showContractModal, setShowContractModal] = useState(false);
   const [rigConfiguration, setRigConfiguration] = useState({
     name: '',
+  const [showMiningPoolModal, setShowMiningPoolModal] = useState(false);
     payoutAddress: '',
     wattAllowance: '',
     stakingAddress: ''
